1bf2a5fdb0fa2f9eca7792c3648415c8f49d0ce5
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-03-29 11:42:19 -0500

    Agregando Indicadores de Pendiente o Completada

--- src/08-useReducer/components/TodoItem.jsx
+++ src/08-useReducer/components/TodoItem.jsx
@@ -4,7 +4,12 @@ export const TodoItem = ({ todo, onDeleteTodo, onToggleTodo }) => {
 
     return (
         <li className="list-group-item d-flex justify-content-between">
-            <span style={{ cursor: 'pointer', userSelect: 'none' }} onDoubleClick={() => onToggleTodo(todo.id)} className={`align-self-center fw-bold ${(todo.donde) ? 'text-success' : 'text-danger'}`}>{todo.description}</span>
+            <div>
+                <span style={{userSelect: 'none' }} className="align-self-center fw-bold me-2">{todo.description}</span>
+                {
+                    <span onDoubleClick={() => onToggleTodo(todo.id)} className={todo.done ? "btn btn-success" : 'btn btn-danger'}>{todo.done ? 'Completada' : 'Pendiente'}</span>
+                }
+            </div>
             <button onClick={() => onDeleteTodo(todo.id)} className="btn btn-danger ">X</button>
         </li>
     )
