c65138b75a08539cd8f3ddb37ba98e5a21229c40
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-03-29 14:52:15 -0500

    Configurando las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-
+import { BrowserRouter } from "react-router-dom";
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterCustomHook } from './01-useState/CounterCustomHook';
@@ -16,12 +16,15 @@ import './index.css'
 // import { CallBackHook } from './06-memos/CallBackHook';
 // import { Padre } from './07-tarea-memo/Padre';
 // import './08-useReducer/intro-reducer';
-
-import { TodoApp } from './08-useReducer/TodoApp';
+// import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
 
-    <TodoApp />
-  /* </React.StrictMode>, */
+  <BrowserRouter>
+
+    <MainApp />
+  </BrowserRouter>
+  /* </React.StrictMode> */
 )
